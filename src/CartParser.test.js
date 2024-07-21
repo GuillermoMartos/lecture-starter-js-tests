d2b6a0eb0e658b63a8eac0f2be4f9dae5d665a26
Guillermo Martos <guille.l.martos@gmail.com>
2024-07-22 00:51:33 +0200

    - + fix refactor validate units tests

--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -5,6 +5,7 @@ const cartCSVFilePath = path.join(__dirname, '..', 'samples', 'cart.csv');
 const allValidationErrorsCSVFilePath = path.join(__dirname, '..', 'samples', 'all-validate-errors-cart.csv');
 let parser;
 let stringifiedCSV;
+let allValidationErrorsCartCSV;
 
 describe('All CartParser tests with +65 coverage expected', () => {
 	beforeEach(() => {
@@ -14,21 +15,41 @@ describe('All CartParser tests with +65 coverage expected', () => {
 	describe('CartParser - unit tests', () => {
 	  beforeEach(() => {
 		stringifiedCSV = parser.readFile(cartCSVFilePath);
+		allValidationErrorsCartCSV = parser.readFile(allValidationErrorsCSVFilePath);
 	  });
   
 	  test('when CSV data input, readFile returns a string from CSV', () => {
 		expect(stringifiedCSV).toContain('Mollis consequat,9.00,3');
 	  });
 		
-	  test('when cart CSV data input has each invalid error type, validate function return array with each error type', () => {
-		  stringifiedCSV = parser.readFile(allValidationErrorsCSVFilePath);
-		  const validationsArray = parser.validate(stringifiedCSV)
+	  test('when cart CSV data input has header error, validate function return array with proper type', () => {
+		  const validationsArray = parser.validate(allValidationErrorsCartCSV)
 
 		  expect(validationsArray).toHaveLength(4)
 		  expect(validationsArray[0].type).toEqual('header')
+
+	  });
+		
+	  test('when cart CSV data input has row error, validate function return array with proper type', () => {
+		  const validationsArray = parser.validate(allValidationErrorsCartCSV)
+
+		  expect(validationsArray).toHaveLength(4)
 		  expect(validationsArray[1].type).toEqual('row')
+
+	  });
+		
+	  test('when cart CSV data input has cell error, validate function return array with proper type and empty message error', () => {
+		  const validationsArray = parser.validate(allValidationErrorsCartCSV)
+
+		  expect(validationsArray).toHaveLength(4)
 		  expect(validationsArray[2].type).toEqual('cell')
 		  expect(validationsArray[2].message).toContain('Expected cell to be a nonempty string but received')
+	  });
+		
+	  test('when cart CSV data input has cell error, validate function return array with proper type and negative message error', () => {
+		  const validationsArray = parser.validate(allValidationErrorsCartCSV)
+
+		  expect(validationsArray).toHaveLength(4)
 		  expect(validationsArray[3].type).toEqual('cell')
 		  expect(validationsArray[3].message).toContain('Expected cell to be a positive number but received')
 	  });
@@ -72,6 +93,5 @@ describe('All CartParser tests with +65 coverage expected', () => {
 			expect(cartSerialized.items).toHaveLength(5)
 			expect(cartSerialized.total).toEqual(400.44)
 		})
-		
 	});
   });
