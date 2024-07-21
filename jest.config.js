6fa74c433235a1a65fd65b05fdbdb6685d7b98f8
Guillermo Martos <guille.l.martos@gmail.com>
2024-07-21 21:58:38 +0200

    - + fist commit configurations + feat add unit rest readFile + feat add unit test validate positive

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    collectCoverage: true,
+    coverageThreshold: {
+      global: {
+        statements: 65,
+        branches: 65,
+        functions: 65,
+        lines: 65,
+      },
+    },
+  };
+  
--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -1,15 +1,33 @@
 import CartParser from './CartParser';
+import path from 'path'
 
+const cartCSVFilePath = path.join(__dirname, '..', 'samples', 'cart.csv');
 let parser;
+let stringifiedCSV;
 
-beforeEach(() => {
-	parser = new CartParser();
-});
-
-describe('CartParser - unit tests', () => {
-	// Add your unit tests here.
-});
-
-describe('CartParser - integration test', () => {
-	// Add your integration test here.
-});
+describe('All CartParser tests with +65 coverage expected', () => {
+	beforeEach(() => {
+	  parser = new CartParser();
+	});
+  
+	describe('CartParser - unit tests', () => {
+	  beforeEach(() => {
+		stringifiedCSV = parser.readFile(cartCSVFilePath);
+	  });
+  
+	  test('when CSV data input, readFile returns a string from CSV', () => {
+		expect(stringifiedCSV).toContain('Mollis consequat,9.00,3');
+	  });
+		
+	  test('when cart CSV data input is ok, validate function return empty array', () => {
+		  stringifiedCSV = parser.readFile(cartCSVFilePath);
+		  const validationsArray = parser.validate(stringifiedCSV)
+		  
+		  expect(validationsArray).toHaveLength(0)
+	  });
+	});
+  
+	describe('CartParser - integration test', () => {
+	  // Add your integration test here.
+	});
+  });
